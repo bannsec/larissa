2c97751459bad4a57b30fa4625dba39e55967259
Owlz <whootandahalf@gmail.com>
2017-05-24 16:59:21 -0400

    Adding extra search path for boost

--- setup.py
+++ setup.py
@@ -38,6 +38,13 @@ def _get_boost_path():
         if _is_boost_new_enough(os.path.join(sys.prefix,"include")):
             return sys.prefix
 
+    # Is it already installed in the virtualenv?
+    # Alternate location
+    if os.path.isdir(os.path.join(sys.prefix, "boost", "include", "boost")):
+        # Is it new enough?
+        if _is_boost_new_enough(os.path.join(sys.prefix,"boost", "include")):
+            return os.path.join(sys.prefix, "boost")
+
     return None
 
 def _get_boost_version(boost_path):
